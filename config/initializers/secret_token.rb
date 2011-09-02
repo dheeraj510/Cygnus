1f15dc6d07e2fdc176ae2c75ba1756a94f30f30e
Randy <randy@astonishinc.com>
2011-09-02 10:43:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cygnus::Application.config.secret_token = '0ad588570f0004828b287599739a4e0a6b6860163626711037cb9e78dee5bd6c95be79f834301f145ea039da921e8c3e135705d2d1a5621a4acdec81877b91cc'
